035124a81f1f01812cae095c077fa3a0046655b7
yahya <yahyahindioglu@gmail.com>
2024-07-06 16:44:23 +0300

    event dispatch action added

---
+++ src/Resources/Action/EventDispatchAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Flutterping\Resources\Action;
+
+class EventDispatchAction extends Action
+{
+    protected $stateId;
+    protected $name;
+    protected $payload;
+
+    public function __construct(string $stateId, string $name, $payload = "")
+    {
+        parent::__construct();
+        $this->stateId = $stateId;
+        $this->name = $name;
+        $this->payload = $payload;
+    }
+
+    public function getActionType(): string
+    {
+        return 'EventDispatchAction';
+    }
+}
